6885e935f17da5a7c7418b1b0e3668e08cc85fa6
Aditya Chakraborty <adityachakra16@gmail.com>
2022-08-22 10:27:49 -0500

    fix build

--- src/retro/commands/impl/index.ts
+++ src/retro/commands/impl/index.ts
@@ -3,4 +3,3 @@ export * from './update-retro.command';
 export * from './update-retro-vote.command';
 export * from './end-retro.command';
 export * from './add-feedback.command';
-export * from './update-retro-payment.command';
