c7b243d38398e5b05401ef16fc4f82df164563ac
TheReshma <reshmashaik3011@gmail.com>
2023-02-10 03:33:23 +0530

    fix: mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -57,8 +57,7 @@ import { AdvancedAccessService } from './collection/services/advanced-access.ser
 import { MintKudosService } from './credentials/services/mintkudos.service';
 import { NotificationModule } from './notification/notification.module';
 
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost:27017/nest';
+const databaseUrl = `${process.env.MONGO_URL}/nest?authSource=admin&retryWrites=true&w=majority`;
 @Module({
   imports: [
     TypegooseModule.forRoot(databaseUrl),
