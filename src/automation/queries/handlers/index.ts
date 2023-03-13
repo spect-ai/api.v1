b3054b61d415639f6114625a86b02bc380ba61ac
adityachakra16 <adityachakra16@gmail.com>
2023-03-13 17:15:12 +0530

    export trigger handler

--- src/automation/queries/handlers/index.ts
+++ src/automation/queries/handlers/index.ts
@@ -1,7 +1,9 @@
+import { GetTriggeredCollectionAutomationsQueryHandler } from './get-triggered-automation.handler';
 import { HasSatisfiedDataConditionsQueryHandler } from './has-satisfied-conditions-v2.handler';
 import { IsTriggeredSelectFieldQueryHandler } from './is-triggered-v2.handler';
 
 export const QueryHandlers = [
   IsTriggeredSelectFieldQueryHandler,
   HasSatisfiedDataConditionsQueryHandler,
+  GetTriggeredCollectionAutomationsQueryHandler,
 ];
